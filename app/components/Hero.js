0e2bb55a02ba7d02e54ab6c79c0a1415fd23596c
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-07 17:12:37 +0300

    add container to hero

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import NavBar from './NavBar'
 
 function Hero() {
   return (
@@ -11,6 +12,17 @@ function Hero() {
               height={600}
               alt="hero"
           /> 
+          <NavBar />
+          <div className="container grid items-center h-[calc(100vh-120px)] p-4">
+              <div className="w-fit p-4 space-y-4 bg-[#ffffff98]">
+                  <p className='uppercase font-medium font-mono'>Lorem ipsum dolor sit amet</p>
+                  <h2 className='text-4xl sm:text-6xl font-bold'>
+                      Lorem <span className='text-blue-500'>ipsum</span>
+                  </h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, at!</p>
+                  <button className='bg-blue-500 hover:bg-blue-700 text-white font-normal py-2 px-4 rounded'>View More</button>
+              </div>
+          </div>
       </div>
   )
 }
