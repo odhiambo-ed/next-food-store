d3b98e1983767a596c296f896f4d3783450b910a
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-07 17:12:12 +0300

    edit main

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Hero from './components/Hero'
-import NavBar from './components/NavBar'
 
 export default function Home() {
   return (
     <main className='p-0 m-0'>
-      <NavBar />
       <Hero />
     </main>
   )
